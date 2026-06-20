f41d8ed91c83df58b1bcee392739a2723219b227
mtemer <49280476+mtemer@users.noreply.github.com>
2026-06-20 02:55:44 +0200

    Add files via upload

---
+++ TodoItem.cs
@@ -0,0 +1,12 @@
+﻿public class TodoItem
+{
+    // 1. DODAJTE OVAJ REDAK (EF Core će ovo automatski prepoznati kao Primarni Ključ)
+    // Nakon dodavanja novog polja u shell pokrenuti naredbu: dotnet ef migrations add AddIdToTodoItem i dotnet ef database update
+    public int Id { get; set; }
+    public string? Sifra { get; set; }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+    public DateTime? DueDate { get; set; }
+    public byte[]? PdfContent { get; set; }
+    public string? PdfFileName { get; set; }
+}
